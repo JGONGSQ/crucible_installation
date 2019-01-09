cd7e1a8c4c984d2ac07947a818a30769a83377d7
James <james.gong@accesshq.com>
2019-01-09 16:05:13 +1100

    Minro refactor on the code

--- install.sh
+++ install.sh
@@ -2,30 +2,32 @@
 
 APP_USER = "atlcrucible"
 APP_GROUP = "ndiaatlassian"
-APP_INSTALL_PATH = "/opt/atlassian/crucible"
-APP_DATA_DIR = "/data/atlcrucible"
-JAVA_HOME = "/usr/lib/jvm/jre-1.8.0-openjdk"
-ENV_PATH = "/etc/environment"
-SERVICE_PATH = "/usr/lib/systemd/system/crucible.service"
-
-
-# in some server, you might need to download the wget, unzip, java as well,
-# `yum install wget`
-# `yum install unzip`
-# `yum install java-1.8.0-openjdk`
-# `yum install httpd` # this may not be needed
-# if not finding the url base, do `dhclient`
-
-# make sure the java_home is defined in the linux env
-echo JAVA_HOME=$JAVA_HOME >> $ENV_PATH
-echo FISHEYE_INST=$APP_DATA_DIR >> $ENV_PATH
 
 sudo groupadd $APP_GROUP
 sudo useradd -g $APP_GROUP $APP_USER
 
 # make the install and data directory with a path
+APP_INSTALL_PATH = "/opt/atlassian/crucible"
+APP_DATA_DIR = "/data/atlcrucible"
+
 mkdir -p $APP_INSTALL_PATH
 mkdir -p $APP_DATA_DIR
+
+# in some server, you might need to download the wget, unzip, java as well,
+yum install wget
+yum install unzip
+yum install java-1.8.0-openjdk
+yum install httpd # this may not be needed
+# if not finding the url base, do `dhclient`
+
+
+JAVA_HOME = "/usr/lib/jvm/jre-1.8.0-openjdk"
+ENV_PATH = "/etc/environment"
+SERVICE_PATH = "/usr/lib/systemd/system/crucible.service"
+
+# make sure the java_home is defined in the linux env
+echo JAVA_HOME=$JAVA_HOME >> $ENV_PATH
+echo FISHEYE_INST=$APP_DATA_DIR >> $ENV_PATH
 
 # donwload the crucible
 wget https://www.atlassian.com/software/crucible/downloads/binary/crucible-4.6.1.zip
@@ -41,8 +43,7 @@ chown --recursive $APP_USER:$APP_GROUP $APP_DATA_DIR
 cp $APP_INSTALL_PATH/config.xml $APP_DATA_DIR
 
 cp atlcrucible.service $SERVICE_PATH
-
-chmod 751 $SERVICE_PATH
+chmod 644 $SERVICE_PATH
 chown --recursive $APP_USER:$APP_GROUP $SERVICE_PATH
 systemctl daemon-reload
 
@@ -52,11 +53,11 @@ systemctl daemon-reload
 # e.g.
 # `firewall-cmd --list-port`
 # if not, need to open the 8060 port by:
-# `firewall-cmd --permanent --add-service=http`
-# `firewall-cmd --permanent --add-port=8060/tcp`
-# `firewall-cmd --reload`
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-port=8060/tcp
+firewall-cmd --reload
 
 # lastly, start the crucible service
-# `service crucible start` 
+service crucible start
 # or start the service as other user 
 # `runuser -l atlcrucible -c 'service crucible start'`
